fd56c2382eb24dea3a70695299d568a2e73a4688
vishalw3villa <vishalprakash.maurya@w3vila.com>
2023-07-11 17:30:05 +0530

    second commit

---
+++ assets/script/style.js
@@ -0,0 +1,206 @@
+$(".hero_sub1").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 1,
+           
+        },
+        1000: {
+            items: 1,
+        }
+    }
+})
+
+
+$(".about1_sub2_sub2").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 2,
+           
+        },
+        1000: {
+            items: 3,
+        },
+        1200:{
+            items: 5,
+        }
+    }
+})
+
+
+$(".about2_sub2_sub2").owlCarousel({
+    loop: true,
+    nav: false,
+    margin:15,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 1,
+           
+        },
+        1000: {
+            items: 3,
+        },
+        1200: {
+            items: 4,
+        }
+    }
+})
+
+$(".about3_sub2_sub2").owlCarousel({
+    loop: true,
+    margin:30, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 1,
+           
+        },
+        1000: {
+            items: 3,
+        },
+        1200: {
+            items: 4,
+        }
+    }
+})
+
+
+$("#shop").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 3,
+           
+        },
+        1000: {
+            items: 6,
+        },
+        1200: {
+            items: 9,
+        }
+    }
+})
+
+
+$("#about_6_card").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 2,
+           
+        },
+        1000: {
+            items: 2,
+        },
+        1200: {
+            items: 3,
+        },
+    }
+})
+
+
+$("#about_7_carousel").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 2,
+           
+        },
+        1000: {
+            items: 2,
+        },
+        1200: {
+            items: 3,
+        }
+    }
+})
+
+$(".about8_sub2").owlCarousel({
+    loop: true,
+    margin:10, 
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        600: {
+            items: 2,
+           
+        },
+        1000: {
+            items: 3,
+        },
+        1200: {
+            items: 4,
+        },
+    }
+})
+
+
+
+
+
