5f5d39f6309b77e753e75dc532eb8ac12b834526
vishalw3villa <vishalprakash.maurya@w3vila.com>
2023-08-09 17:08:09 +0530

    upgrade file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,60 @@
 
-// *****************************************************************Script of landing Page
 
-let aboutOneImages = {
-    "TOP CREATION": [
-        "./assets/images/fashion.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/health&beuty.avif",
-        "./assets/images/footwear.avif",
-        "./assets/images/homedecor.avif",
-    ],
-    "ELECTRONICS": [
-        "./assets/images/electronicsHeadphone.avif",
-        "./assets/images/electonicsGame.avif",
-        "./assets/images/electonicsCameracheckbox_bar.avif",
-        "./assets/images/electronicsEarpods.avif",
-        "./assets/images/electonicsCamera.avif",
-    ],
-    "BEAUTY": [
-        "./assets/images/beautyAcces.avif",
-        "./assets/images/beautyLip.avif",
-        "./assets/images/beautyBody.avif",
-        "./assets/images/beautyMascara.avif",
-        "./assets/images/beautyMakeup.avif",
-    ],
-    "FASHION": [
-        "./assets/images/fashionAssco.avif",
-        "./assets/images/fashionDress.avif",
-        "./assets/images/fashionPant.avif",
-        "./assets/images/fashionTshirt.avif",
-        "./assets/images/fashionTop.avif",
-    ]
-};
+async function addHeaderInIndex() {
+    try {
+        let response = await fetch("./header.html");
+        let data = await response.text();
+        document.getElementById('containheaderindex').innerHTML = data;
+        loadJS("./assets/js/login.js");
+        searchInput();
+    }
+    catch { error => console.error('Error fetching content:', error) };
+}
 
-let aboutOneTags = {
-    "TOP CREATION": [
-        "Fashion",
-        "Bags",
-        "Health & Beauty",
-        "Footwear",
-        "Home Decor",
-    ],
-    "ELECTRONICS": [
-        "Headfone",
-        "Game",
-        "Camera",
-        "Earpod",
-        "Camera",
-    ],
-    "BEAUTY": [
-        "Accesories",
-        "Lipstic",
-        "Body",
-        "Mascara",
-        "Makeup",
-    ],
-    "FASHION": [
-        "Accesories",
-        "Dresses",
-        "Pant",
-        "T-shirt",
-        "Top",
-    ]
-};
+addHeaderInIndex();
+
+function loadJS(FILE_URL, async = true) {
+    let scriptEle = document.createElement("script");
+
+    scriptEle.setAttribute("src", FILE_URL);
+    scriptEle.setAttribute("type", "text/javascript");
+    scriptEle.setAttribute("async", async);
+
+    document.body.appendChild(scriptEle);
+
+    // success event 
+    scriptEle.addEventListener("load", () => {
+        console.log("File loaded")
+    });
+
+    // error event
+    scriptEle.addEventListener("error", (ev) => {
+        console.log("Error on loading file", ev);
+    });
+}
+
+/* ****************
+******Why Buy from Us ****
+*********************/
+
+async function whyBuyUs(clickedBtn, cardOneImg, cardOneBtn) {
+    try {
+        let response = await fetch("./assets/data/whyBuyUs.json");
+        let data = await response.json();
+
+        for (let j = 0; j < cardOneImg.length; j++) {
+            let {
+                img,
+                tag
+            } = data[clickedBtn][j % 5];
+            cardOneImg[j].src = img;
+            cardOneBtn[j].innerHTML = tag;
+        }
+    } catch (error) {
+        console.error("Error fetching or processing data:", error);
+    }
+
+}
 
 
 
@@ -69,17 +62,12 @@ let aboutOneBtn = document.getElementsByClassName("about1_btn");
 
 for (let i = 0; i < aboutOneBtn.length; i++) {
     aboutOneBtn[i].onclick = function () {
-        var clickedImg = this.querySelector('p').textContent;
-        console.log(clickedImg)
+        var clickedBtn = this.querySelector('p').textContent;
         var cardOneImg = document.querySelectorAll(".about1_card img");
-        for (let j = 0; j < cardOneImg.length; j++) {
-            cardOneImg[j].src = aboutOneImages[clickedImg][j % 5];
-        }
-
         var cardOneBtn = document.querySelectorAll(".about1_card button");
-        for (let j = 0; j < cardOneBtn.length; j++) {
-            cardOneBtn[j].innerHTML = aboutOneTags[clickedImg][j % 5];
-        }
+
+        // Fetch json data****
+        whyBuyUs(clickedBtn, cardOneImg, cardOneBtn);
 
         for (let k = 0; k < aboutOneBtn.length; k++) {
             aboutOneBtn[k].style["background-color"] = "hsl(0deg 0% 97.25%)";
@@ -95,42 +83,30 @@ for (let i = 0; i < aboutOneBtn.length; i++) {
         this.style["color"] = "white";
         this.innerHTML += `<div class="about1_arrow_shape"></div>`;
     };
+}
 
+/* ****************
+******Featured Products****
+*********************/
+
+
+async function featuredProduct(clickedImg, cardTwoImg) {
+    try {
+        let response = await fetch("./assets/data/featuredProduct.json");
+        let data = await response.json();
+
+        for (let j = 0; j < cardTwoImg.length; j++) {
+            let {
+                img
+            } = data[clickedImg][j % 5];
+            cardTwoImg[j].src = img;
+        }
+    } catch (error) {
+        console.error("Error fetching or processing data:", error);
+    }
 
 }
 
-
-
-let aboutTwoImages = {
-    "FEATURED": [
-        "./assets/images/electronics.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/fashion.avif",
-        "./assets/images/homedecor.avif",
-    ],
-    "LATEST": [
-        "./assets/images/electronics.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/fashion.avif",
-        "./assets/images/homedecor.avif",
-    ],
-    "BESTSELLERS": [
-        "./assets/images/electronics.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/fashion.avif",
-        "./assets/images/homedecor.avif",
-    ],
-    "SPECIALS": [
-        "./assets/images/electronics.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/bags.avif",
-        "./assets/images/fashion.avif",
-        "./assets/images/homedecor.avif",
-    ]
-};
 
 
 let aboutTwoBtn = document.getElementsByClassName("about2_btn");
@@ -140,11 +116,8 @@ for (let i = 0; i < aboutTwoBtn.length; i++) {
         var clickedImg = this.querySelector('p').textContent;
         console.log(clickedImg)
         var cardTwoImg = document.querySelectorAll(".about2_img img");
-        for (let j = 0; j < cardTwoImg.length; j++) {
-            cardTwoImg[j].src = aboutTwoImages[clickedImg][j % 5];
-        }
 
-
+        featuredProduct(clickedImg, cardTwoImg);
 
         for (let k = 0; k < aboutTwoBtn.length; k++) {
             aboutTwoBtn[k].style["background-color"] = "hsl(0deg 0% 97.25%)";
@@ -161,22 +134,31 @@ for (let i = 0; i < aboutTwoBtn.length; i++) {
         this.innerHTML += `<div class="about2_arrow_shape"></div>`;
 
     };
+}
 
+
+/* ****************
+******Blog Posts****
+*********************/
+
+
+async function blogPosts(clickedImg, cardSixImg) {
+    try {
+        let response = await fetch("./assets/data/blog.json");
+        let data = await response.json();
+
+        for (let j = 0; j < cardSixImg.length; j++) {
+            let {
+                img
+            } = data[clickedImg][j % 3];
+            cardSixImg[j].src = img;
+        }
+    } catch (error) {
+        console.error("Error fetching or processing data:", error);
+    }
 
 }
 
-let aboutSixImages = {
-    "LATEST POST": [
-        "./assets/images/img61.avif",
-        "./assets/images/img62.avif",
-        "./assets/images/img63.avif",
-    ],
-    "MOST READ": [
-        "./assets/images/img63.avif",
-        "./assets/images/img61.avif",
-        "./assets/images/img62.avif",
-    ],
-}
 
 let aboutSixBtn = document.getElementsByClassName("about6_btn");
 
@@ -185,11 +167,7 @@ for (let i = 0; i < aboutSixBtn.length; i++) {
         var clickedImg = this.querySelector('p').textContent;
         console.log(clickedImg)
         var cardSixImg = document.querySelectorAll(".about6_card_img img");
-        for (let j = 0; j < cardSixImg.length; j++) {
-            cardSixImg[j].src = aboutSixImages[clickedImg][j % 3];
-        }
-
-
+        blogPosts(clickedImg, cardSixImg);
 
         for (let k = 0; k < aboutSixBtn.length; k++) {
             aboutSixBtn[k].style["background-color"] = "hsl(0deg 0% 97.25%)";
@@ -208,55 +186,21 @@ for (let i = 0; i < aboutSixBtn.length; i++) {
     };
 }
 
-
-// ***************************************************************************Script for Product page
-
-var aboutProduct = {
-    "Description": "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
-    "Specification": "Specification Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
-    "reviews": "reviews Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
-    "Custom Tabs": "Custom Tabs Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
-}
-
-var aboutProductTab = document.getElementsByClassName("overview_slide");
-
-for (let i = 0; i < aboutProductTab.length; i++) {
-    aboutProductTab[i].onclick = function () {
-        var clickedValue = this.innerHTML;
-        document.getElementById("truncate").innerHTML = aboutProduct[clickedValue];
-
-        for (var j = 0; j < aboutProductTab.length; j++) {
-            aboutProductTab[j].style["border-bottom"] = "0px";
-            aboutProductTab[j].style["color"] = "gray";
-        }
-
-        this.style["border-bottom"] = "2px solid orange";
-        this.style["color"] = "black";
-
-    }
-}
-
-let sapmleImg = [
-    "./assets/images/camera1.avif",
-    "./assets/images/campng.png",
-    "./assets/images/computer.avif",
-    "./assets/images/mobile2.avif",
-    "./assets/images/mobile3.avif",
-    "./assets/images/mobile1.avif"
-]
-
-let sampleImgTab = document.getElementsByClassName("sample_img");
-
-for (let i = 0; i < sampleImgTab.length; i++) {
-    sampleImgTab[i].onclick = function () {
-        var clickedImg = this.innerHTML;
-        document.getElementById("appleproduct").src = sapmleImg[i];
-    }
-}
-
-
 // *******************************************Script of Search Page
 
+
+async function addHeaderInSearch() {
+    try {
+        let response = await fetch("./header.html");
+        let data = await response.text();
+        document.getElementById('containheader').innerHTML = data;
+        loadJS("./assets/js/login.js");
+        searchInput();
+    }
+    catch { error => console.error('Error fetching content:', error) };
+}
+
+addHeaderInSearch();
 
 const buttonList = document.getElementById("showListbtn");
 const buttonGrid = document.getElementById("showGridbtn");
@@ -336,6 +280,144 @@ for (let i = 0; i < minusPlus.length; i++) {
 }
 
 
+/* ************
+***** Search input*****
+************************/
+
+function searchInput() {
+    const searchBtn = document.getElementById("searchbtn");
+    const searchInput = document.getElementById("searchinput");
+
+    if (searchBtn) {
+        searchBtn.addEventListener('click', searchAction)
+        searchInput.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                searchAction();
+            }
+        })
+    }
+    else {
+        console.log(`error searchBtn`);
+    }
+
+    function searchAction() {
+        let searchQuery1 = searchInput.value;
+        if (searchQuery1) {
+            console.log(searchQuery1);
+            const searchPageUrl = `searchPage.html?search=${encodeURIComponent(searchQuery1)}`;
+            window.location.href = searchPageUrl;
+        }
+        else {
+            console.log("error searchAction");
+        }
+    }
+
+
+    const userlParam = new URLSearchParams(window.location.search);
+    const searchQuery = userlParam.get("search");
+
+    if (searchQuery) {
+        fetchSearchandDisplay(searchQuery);
+    }
+
+    function fetchSearchandDisplay(searchQuery) {
+        document.getElementById("searchname").innerText = searchQuery;
+        if (searchQuery != "Camera") {
+            changeSearchUi();
+        }
+    }
+
+    function changeSearchUi() {
+        let leftSearch = document.getElementById("search_left");
+        let hideGrid = document.getElementById("showGrid");
+        let hideShowGridbtn = document.getElementById("hideshowgridbtn");
+        let hideLine = document.getElementById("hideline");
+        let hideContinue = document.getElementById("hidecontinue");
+        let endList = document.getElementById("end_list");
+
+        leftSearch.classList.add("hidesearchleft");
+        hideGrid.classList.add("hidesearchleft");
+        hideLine.classList.add("hidesearchleft");
+        hideLine.classList.add("hidesearchleft");
+        endList.classList.add("hidesearchleft");
+        hideContinue.classList.remove("hidesearchleft");
+        hideShowGridbtn.innerHTML = "There is no product that matches the search criteria.";
+    }
+
+    let hideContinue = document.getElementById("hidecontinue");
+    if (hideContinue) {
+        hideContinue.addEventListener('click', returnHome)
+    }
+
+    function returnHome() {
+        let returnHomeUrl = "index.html";
+        window.location.href = returnHomeUrl;
+    }
+}
+
+
+// ***************************************************************************Script for Product page
+async function addHeaderInProduct() {
+    try {
+        let response = await fetch("./header.html");
+        let data = await response.text();
+        document.getElementById('headerInProduct').innerHTML = data;
+        loadJS("./assets/js/login.js");
+        searchInput();
+    }
+    catch { error => console.error('Error fetching content:', error) };
+}
+
+addHeaderInProduct();
+
+var aboutProduct = {
+    "Description": "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
+    "Specification": "Specification Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
+    "reviews": "reviews Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
+    "Custom Tabs": "Custom Tabs Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapient Hello how ",
+}
+
+var aboutProductTab = document.getElementsByClassName("overview_slide");
+
+for (let i = 0; i < aboutProductTab.length; i++) {
+    aboutProductTab[i].onclick = function () {
+        var clickedValue = this.innerHTML;
+        document.getElementById("truncate").innerHTML = aboutProduct[clickedValue];
+
+        for (var j = 0; j < aboutProductTab.length; j++) {
+            aboutProductTab[j].style["border-bottom"] = "0px";
+            aboutProductTab[j].style["color"] = "gray";
+        }
+
+        this.style["border-bottom"] = "2px solid orange";
+        this.style["color"] = "black";
+
+    }
+}
+
+let sapmleImg = [
+    "./assets/images/camera1.avif",
+    "./assets/images/campng.png",
+    "./assets/images/computer.avif",
+    "./assets/images/mobile2.avif",
+    "./assets/images/mobile3.avif",
+    "./assets/images/mobile1.avif"
+]
+
+let sampleImgTab = document.getElementsByClassName("sample_img");
+
+for (let i = 0; i < sampleImgTab.length; i++) {
+    sampleImgTab[i].onclick = function () {
+        var clickedImg = this.innerHTML;
+        document.getElementById("appleproduct").src = sapmleImg[i];
+    }
+}
+
+
+
+/* ***********************
+*********** Wish List*****
+********************************/
 
 
 
